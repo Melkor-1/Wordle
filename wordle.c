5418b7c5b934d5c0ffc69f8145590e9cce2c65c7
Melkor-1 <harith.bin.salam@gmail.com>
2023-10-17 13:31:23 -0700

    Refactored main() into separate functions.

--- wordle.c
+++ wordle.c
@@ -5,10 +5,11 @@
 #include <time.h>
 
 /* Each of our text files contains 1000 words. */
-#define LIST_SIZE  1000
-#define FNAME_SIZE 6
-
-#define PROMPT_SIZE 128
+#define LIST_SIZE       1000
+#define FNAME_SIZE      6
+#define PROMPT_SIZE     128
+#define MAX_WORD_LENGTH 8
+#define MIN_WORD_LENGTH 5
 
 #define EXACT   2               /* Right letter; right place. */
 #define CLOSE   1               /* Right letter; wrong place. */
@@ -31,7 +32,7 @@ static char *get_guess(size_t wlen)
     snprintf(prompt, PROMPT_SIZE, "Input a %zu-letter word: ", wlen);
 
     do {
-        printf(prompt);
+        fputs(prompt, stdout);
         rc = getline(&guess, &glen, stdin);
 
         if (rc == -1) {
@@ -43,24 +44,49 @@ static char *get_guess(size_t wlen)
     return guess;
 }
 
-static size_t check_word(const char *restrict guess, size_t *restrict status,
-                         const char *restrict choice)
+static bool guess_exists(const char *restrict guess, size_t lsize, size_t wlen, const char list[lsize][wlen]) 
 {
-    size_t score = 0;
+    for (size_t i = 0; i < lsize ; ++i) {
+        if (!strcasecmp(guess, list[i])) {
+            return true;
+        }
+    }
 
-    for (size_t i = 0; guess[i]; ++i) {
+    return false;
+}
+
+static size_t sum_array(size_t size, const size_t arr[size])
+{
+    size_t sum = 0;
+
+    for (size_t i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+static size_t calculate_word_score(const char *restrict guess, size_t *restrict status,
+                                   const char *restrict choice)
+{
+    size_t i;
+
+    for (i = 0; guess[i]; ++i) {
         for (size_t j = 0; choice[j]; ++j) {
             if (choice[j] == guess[i]) {
-                status[i] = (i == j) ? EXACT : CLOSE;
-                score += status[i];
+                if (i == j) {
+                    status[i] = EXACT;
+                } else if (status[i] != EXACT) {
+                    status[i] = CLOSE;
+                }
             }
         }
     }
 
-    return score;
+    return sum_array(i, status);
 }
 
-static void print_word(const char *restrict guess, int wlen,
+static void print_word(const char *restrict guess, size_t wlen,
                        const size_t *restrict status)
 {
     for (size_t i = 0; i < wlen; ++i) {
@@ -79,12 +105,54 @@ static void print_word(const char *restrict guess, int wlen,
         }
     }
     putchar('\n');
-    fflush(stdout);
 }
 
 static inline bool validate_wlen(size_t wlen)
 {
-    return wlen >= 5 && wlen <= 8;
+    return wlen >= MIN_WORD_LENGTH && wlen <= MAX_WORD_LENGTH;
+}
+
+static bool run_game(size_t lsize, size_t wlen, const char options[lsize][wlen + 1], const char *restrict choice)
+{
+    /* Allow one more guess than the length of the word. */
+    const size_t guesses = wlen + 1;
+    bool won = false;
+
+    printf(GREEN "This is WORDLE50" RESET "\n"
+           "You have %zu tries to guess the %zu-letter word I'm thinking of\n",
+           guesses, wlen);
+
+    for (size_t i = 0; i < guesses; ++i) {
+        char *guess = get_guess(wlen);
+
+        /* Check if guess is in the list. */
+        while (!guess_exists(guess, LIST_SIZE, wlen + 1, options)) {
+            puts("The word is not in list.");
+            free(guess);
+            guess = get_guess(wlen);
+        }
+        
+        /* Array to hold guess status. */
+        size_t status[wlen];
+
+        /* Set all elements of status array initially to 0, aka WRONG. */
+        for (size_t i = 0; i < wlen; ++i) {
+            status[i] = 0;
+        }
+
+        size_t score = calculate_word_score(guess, status, choice);
+        printf("Guess %zu: ", i + 1);
+        print_word(guess, wlen, status);
+        free(guess);
+        
+        /* If they guessed it exactly right, set terminate loop. */
+        if (score == EXACT * wlen) {
+            won = true;
+            break;
+        }
+    }
+
+    return won;
 }
 
 int main(int argc, char **argv)
@@ -94,8 +162,8 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    // Open correct file, each file has exactly LISTSIZE words
-    const size_t wlen = (size_t) atoi(argv[1]);
+    /* Open correct file, each file has exactly LIST_SIZE words. */
+    const size_t wlen = (size_t) strtol(argv[1], NULL, 10);
     char wl_filename[FNAME_SIZE];
 
     snprintf(wl_filename, FNAME_SIZE, "%zu.txt", wlen);
@@ -107,68 +175,18 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    /*
-     * Load word file into an array of size LISTSIZE. 
-     */
+    /* Load word file into an array of size LIST_SIZE. */
     char options[LIST_SIZE][wlen + 1];
 
     for (size_t i = 0; i < LIST_SIZE; ++i) {
         fscanf(wlist, "%s", options[i]);
     }
-
-    /*
-     * Pseudorandomly select a word for this game. 
-     */
+    
+    /* Pseudorandomly select a word for this game. */
     srand(time(NULL));
-    char *const choice = options[rand() % LIST_SIZE];
+    const char *const choice = options[rand() % LIST_SIZE];
 
-    /*
-     * Allow one more guess than the length of the word. 
-     */
-    const size_t guesses = wlen + 1;
-    bool won = false;
-
-    printf(GREEN "This is WORDLE50" RESET "\n"
-           "You have %zu tries to guess the %zu-letter word I'm thinking of\n",
-           guesses, wlen);
-
-    /*
-     * Main game loop, one iteration for each guess. 
-     */
-    for (size_t i = 0; i < guesses; ++i) {
-        /*
-         * Obtain user's guess. 
-         */
-        char *const guess = get_guess(wlen);
-
-        /*
-         * Array to hold guess status, initially set to zero. 
-         */
-        size_t status[wlen];
-
-        /*
-         * Set all elements of status array initially to 0, aka WRONG. 
-         */
-        memset(status, WRONG, wlen);
-
-        /*
-         * Calculate score for the guess. 
-         */
-        size_t score = check_word(guess, status, choice);
-
-        printf("Guess %zu: ", i + 1);
-        print_word(guess, wlen, status);
-        fflush(stdout);
-        free(guess);
-        /*
-         * If they guessed it exactly right, set terminate loop. 
-         */
-        if (score == EXACT * wlen) {
-            won = true;
-            break;
-        }
-    }
-
-    won ? puts("You won!") : printf("The target word was: %s\n", choice);
+    run_game(LIST_SIZE, wlen, options, choice) ? puts("You won!") : printf("The target word was: %s\n", choice);
+    fclose(wlist);
     return EXIT_SUCCESS;
 }
